bae07720c9b909efd85230349210e342be947f5e
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-03-14 11:59:28 -0600

    Add display of correct minutes on the clock

--- main.js
+++ main.js
@@ -4,15 +4,19 @@ const secondHand = document.querySelector('.second-hand');
 
 function setDate() {
     const now = new Date();
-    const seconds = now.getSeconds();
-    const secondsDegrees = ((seconds / 60) * 360) + 90;
-
+    
     const hours = now.getHours();
     const hoursDegrees = ((hours / 60) * 360);
 
+    const minutes = now.getMinutes();
+    const minutesDegrees = ((minutes / 60) * 360) + 90;
+
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
     
-    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
     hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+    minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 }
 
 
