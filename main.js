a6832119161034670ab1e8a43f07fe53482ac28a
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-03-14 12:04:08 -0600

    Change hoursDegrees to divide by 12

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ const secondHand = document.querySelector('.second-hand');
 
 function setDate() {
     const now = new Date();
-    
+
     const hours = now.getHours();
-    const hoursDegrees = ((hours / 60) * 360);
+    const hoursDegrees = ((hours / 12) * 360) + 90;
 
     const minutes = now.getMinutes();
     const minutesDegrees = ((minutes / 60) * 360) + 90;
